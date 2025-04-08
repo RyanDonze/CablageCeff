40d7e87a47fa5e3d1c68034f8df78e535a9f9aae
Ryan Donzé <ryan.donze@ceff.ch>
2025-04-08 15:37:03 +0200

    Design Terminal for the MainWindow and the new page PatchWindow, link between the two pages is done but not available, waiting for data from Excel

--- CâblageCeff/ViewModels/MainWindowViewModel.cs
+++ CâblageCeff/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,32 @@
-﻿namespace CâblageCeff.ViewModels
+﻿using Avalonia;
+using Avalonia.Styling;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+
+namespace CâblageCeff.ViewModels
 {
     public partial class MainWindowViewModel : ViewModelBase
     {
         public string Greeting { get; } = "Welcome to Avalonia!";
+
+        public delegate Task ShowPatchDialogFunc();
+        public ShowPatchDialogFunc ShowPatchDialog { get; set; } = null!;
+
+        [RelayCommand]
+        private async Task OpenPatchScreen()
+        {
+            if (ShowPatchDialog == null)
+                return;
+            await ShowPatchDialog();
+        }
+
+        [RelayCommand]
+        private void ChangeTheme()
+        {
+            Application.Current!.RequestedThemeVariant = Application.Current.RequestedThemeVariant == ThemeVariant.Light
+                ? ThemeVariant.Dark
+                : ThemeVariant.Light;
+            //System.Environment.Exit(0);
+        }
     }
 }
---
+++ CâblageCeff/ViewModels/PatchWindowViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CâblageCeff.ViewModels
+{
+    public partial class PatchWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
+
